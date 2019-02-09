9e0f6e0e1577f0f4e6d7dd766429a64d0c1ad839
tomasz0 <46450682+tomasz0@users.noreply.github.com>
2019-02-09 22:36:18 +0100

    Update exc12.cpp

--- exc12.cpp
+++ exc12.cpp
@@ -12,6 +12,8 @@
 #include<list>
 #include<set>
 using namespace std;
+vector<int> v2;
+vector<int> v;
 int  M, p;
 long long int N;
 int main(){
@@ -19,31 +21,25 @@ int main(){
     cin >> N;
     cout << "Set a range of drawing from 0 to M. M: ";
     cin >> M;
-    vector<int> v;
     random_device rd; 
     mt19937 gen(rd()); 
     uniform_int_distribution<> dis(0, M);
-    for (int s = 0; s<N; ++s)
+    for (int s = 0; s<N; ++s){
        v. push_back(dis(gen));
-       vector<int> v2;
+    }
     for(auto i: v){
-        for (int j = 2; j<=sqrt(i);j++) {
+        for (int j = 2; j<=sqrt(i);j++){
 		p = 0;
-		if (i%j == 0) {
-			p=1;
+		if (i % j == 0){
+			p = 1;
 		 	break;
 		}
-	}
-	if (p == 0 && i != 0 && i != 1)
-	    v2.push_back(i);
-    }
- set<int> k(v2.begin(), v2.end()); 
- cout<<"=======primary digits ========"<<endl;
- for(auto it: k) cout << it << " ";
+	 }
+	 if ((p == 0) && (i > 1)) v2.push_back(i);
+    }	
+    set<int> k(v2.begin(), v2.end()); 
+    cout<<"primary digits: ";
+    for(auto it: k) cout << it << " ";
   
-cout<<endl;
-cout<<endl;
-cout<<"=======drawn================"<<endl;
-  for(auto it2: v) cout<<it2<< " ";
 return 0;
 }

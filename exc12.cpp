1ae4d6b3954b7f8dd6053f2b89575e9f471160f5
tomasz0 <46450682+tomasz0@users.noreply.github.com>
2019-02-10 19:22:57 +0100

    Update exc12.cpp

--- exc12.cpp
+++ exc12.cpp
@@ -1,45 +1,52 @@
 #include<iostream>
-#include<ctime>
 #include<algorithm>
-#include<iterator>
-#include<cstdlib>
-#include<map>
-#include<vector>
-#include<random>
-#include<functional>
-#include<numeric>
 #include<cmath>
-#include<list>
+#include<vector>
 #include<set>
+#include<functional>
+#include<random>
+#include<iterator>
+#include<map>
 using namespace std;
-vector<int> v2;
-vector<int> v;
-int  M, p;
+vector<int> vector_1;
+vector<int> vector_2;
+int  M, p, s;
 long long int N;
 int main(){
     cout << "How many values will be set randomly? N: ";
     cin >> N;
     cout << "Set a range of drawing from 0 to M. M: ";
     cin >> M;
-    random_device rd; 
-    mt19937 gen(rd()); 
+    random_device rd;
+    mt19937 gen(rd());
     uniform_int_distribution<> dis(0, M);
-    for (int s = 0; s<N; ++s){
-       v. push_back(dis(gen));
+    for (auto s = 0; s < N; ++s){
+        vector_1.push_back(dis(gen));
     }
-    for(auto i: v){
-        for (int j = 2; j<=sqrt(i);j++){
-		p = 0;
-		if (i % j == 0){
-			p = 1;
-		 	break;
-		}
-	 }
-	 if ((p == 0) && (i > 1)) v2.push_back(i);
-    }	
-    set<int> k(v2.begin(), v2.end()); 
-    cout<<"primary digits: ";
-    for(auto it: k) cout << it << " ";
-  
+    set<int> k(vector_1.begin(), vector_1.end());
+    k.erase(0);
+    for(int i=2; i<M; i++){
+        for(int j=2; j<=sqrt(i); j++){
+            p=0;
+            if ( i%j == 0 ){
+                p = 1;
+                break;
+            }
+        }
+           if ((p==0) && (i>1)){
+           vector_2.push_back(i);
+           }
+    }
+    map<int,int> mapa = { };
+    for(auto it: vector_2){
+             cout<< it << " ====> ";
+        for(auto it2: k){
+             if(it2 % it == 0){
+                   mapa.emplace(it, it2);
+                   cout << it2 <<" ";
+              }
+        }
+           cout<<endl;
+    }
 return 0;
 }
